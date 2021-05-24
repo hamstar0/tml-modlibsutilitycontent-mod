670975fba3ad452785cbb69fe3d0f5e2c276bbe3
hamstar0 <webutek@gmail.com>
2021-05-24 16:07:29 -0700

    v1.0.0 - Added coal tile to coal item

---
+++ ModLibsUtilityContent/MyItem.cs
@@ -0,0 +1,33 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ModLibsUtilityContent.Tiles;
+
+
+namespace ModLibsUtilityContent {
+	/// @private
+	class ModHelpersItem : GlobalItem {
+		public override void SetDefaults( Item item ) {
+//DataStore.Add( DebugHelpers.GetCurrentContext()+"_"+item.whoAmI+":"+item.type+"_A", 1 );
+			if( item.type == ItemID.Coal ) {
+				if( ModLibsUtilityContentConfig.Instance.CoalAsTile ) {
+					item.maxStack = 999;
+					item.rare = ItemRarityID.Green;
+					item.value = 1000;
+
+					item.useStyle = ItemUseStyleID.SwingThrow;
+					item.useTurn = true;
+					item.useAnimation = 15;
+					item.useTime = 10;
+					item.autoReuse = true;
+
+					item.consumable = true;
+					item.createTile = ModContent.TileType<CoalTile>();
+				}
+			}
+
+			base.SetDefaults( item );
+//DataStore.Add( DebugHelpers.GetCurrentContext()+"_"+item.whoAmI+":"+item.type+"_B", 1 );
+		}
+	}
+}
