c37289187a7cce818ef87ad6852c7a6106b76135
hamstar0 <webutek@gmail.com>
2022-02-11 04:15:15 -0800

    v1.1.0 - Added Magitech Scrap (item), Fallen Cyborg (tile)

---
+++ ModLibsUtilityContent/Items/MagitechScrapItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ModLibsCore.Services.Timers;
+
+
+namespace ModLibsUtilityContent.Items {
+	public partial class MagitechScrapItem : ModItem {
+		public override void SetStaticDefaults() {
+			this.DisplayName.SetDefault( "Magitech Scrap" );
+			this.Tooltip.SetDefault( "Assorted machine parts with assorted enchantments" );
+		}
+
+		public override void SetDefaults() {
+			this.item.width = 12;
+			this.item.height = 12;
+			this.item.maxStack = 1;
+			this.item.consumable = true;
+			this.item.value = Item.buyPrice( 0, 3, 0, 0 );
+			//this.item.UseSound = SoundID.Item108;
+			this.item.rare = ItemRarityID.Orange;
+		}
+	}
+}
---
+++ ModLibsUtilityContent/Tiles/FallenCyborgTile.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+
+namespace ModLibsUtilityContent.Tiles {
+	public class FallenCyborgTile : ModTile {
+		public override void SetDefaults() {
+			Main.tileLighted[ this.Type ] = true;
+			Main.tileFrameImportant[ this.Type ] = true;
+			Main.tileObsidianKill[ this.Type ] = true;
+			//Main.tileNoAttach[ this.Type ] = true;
+
+			TileObjectData.newTile.CopyFrom( TileObjectData.Style2xX );
+			TileObjectData.addTile( this.Type );
+
+			ModTranslation name = this.CreateMapEntryName();
+			name.SetDefault( "Fallen Cyborg" );
+
+			this.AddMapEntry( new Color(160, 160, 180), name );
+
+			this.dustType = 1;
+			this.disableSmartCursor = true;
+			this.adjTiles = new int[] { this.Type };
+		}
+
+
+		public override void NumDust( int i, int j, bool fail, ref int num ) {
+			num = fail ? 1 : 3;
+		}
+	}
+}
