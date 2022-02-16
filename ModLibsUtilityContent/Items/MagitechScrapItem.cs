f4e9d7a5ee1335f1a28f424c5c36a4f1e16dcce8
hamstar0 <webutek@gmail.com>
2022-02-16 03:22:23 -0800

    v1.1.0 - Added API hooks to Magitech Scrap, Fallen Cyborg

--- ModLibsUtilityContent/Items/MagitechScrapItem.cs
+++ ModLibsUtilityContent/Items/MagitechScrapItem.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using ModLibsCore.Services.Timers;
 
 
 namespace ModLibsUtilityContent.Items {
+	/// <summary></summary>
 	public partial class MagitechScrapItem : ModItem {
+		public delegate void SetStaticDefaults_Deleg( MagitechScrapItem myitem );
+
+		public delegate void SetDefaults_Deleg( MagitechScrapItem myitem );
+
+		public delegate void ModifyTooltips_Deleg( MagitechScrapItem myitem, List<TooltipLine> tooltips );
+
+		public delegate bool CanRightClick_Deleg( MagitechScrapItem myitem );
+
+
+
+		////////////////
+
+		public SetStaticDefaults_Deleg SetStaticDefaults_Hook;
+
+		public SetDefaults_Deleg SetDefaults_Hook;
+
+		public ModifyTooltips_Deleg ModifyTooltips_Hook;
+
+		public CanRightClick_Deleg CanRightClick_Hook;
+
+
+
+		////////////////
+
 		/// @private
 		public override string Texture => ModLibsUtilityContentMod.Instance.Name + "/Items/MagitechScrapItem";
 
@@ -19,6 +43,10 @@ namespace ModLibsUtilityContent.Items {
 		public override void SetStaticDefaults() {
 			this.DisplayName.SetDefault( "Magitech Scrap" );
 			this.Tooltip.SetDefault( "Assorted machine parts with assorted enchantments" );
+
+			//
+
+			this.SetStaticDefaults_Hook?.Invoke( this );
 		}
 
 		/// @private
@@ -30,6 +58,20 @@ namespace ModLibsUtilityContent.Items {
 			this.item.value = Item.buyPrice( 0, 3, 0, 0 );
 			//this.item.UseSound = SoundID.Item108;
 			this.item.rare = ItemRarityID.Orange;
+
+			//
+
+			this.SetDefaults_Hook?.Invoke( this );
+		}
+
+		////
+
+		public override void ModifyTooltips( List<TooltipLine> tooltips ) {
+			this.ModifyTooltips_Hook?.Invoke( this, tooltips );
+		}
+
+		public override bool CanRightClick() {
+			return this.CanRightClick_Hook?.Invoke( this ) ?? false;
 		}
 	}
 }
--- ModLibsUtilityContent/Tiles/FallenCyborgTile.cs
+++ ModLibsUtilityContent/Tiles/FallenCyborgTile.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
@@ -6,6 +7,22 @@ using Terraria.ObjectData;
 
 namespace ModLibsUtilityContent.Tiles {
 	public class FallenCyborgTile : ModTile {
+		public delegate void KillMultiTile_Deleg( int i, int j, int frameX, int frameY );
+
+		public delegate void MouseOverFar_Deleg(int i, int j);
+
+
+
+		////////////////
+
+		public KillMultiTile_Deleg KillMultiTile_Hook;
+
+		public MouseOverFar_Deleg MouseOverFar_Hook;
+
+
+
+		////////////////
+
 		/// @private
 		public override bool Autoload( ref string name, ref string texture ) {
 			texture = ModLibsUtilityContentMod.Instance.Name + "/Tiles/FallenCyborgTile";
@@ -37,6 +54,17 @@ namespace ModLibsUtilityContent.Tiles {
 		/// @private
 		public override void NumDust( int i, int j, bool fail, ref int num ) {
 			num = fail ? 1 : 3;
+		}
+
+
+		////////////////
+
+		public override void KillMultiTile( int i, int j, int frameX, int frameY ) {
+			this.KillMultiTile_Hook?.Invoke( i, j, frameX, frameY );
+		}
+
+		public override void MouseOverFar( int i, int j ) {
+			this.MouseOverFar_Hook?.Invoke( i, j );
 		}
 	}
 }
